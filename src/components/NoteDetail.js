3822b09a89962e6443d45d9d4bc6cb52c244bcf1
Nicole <nicole@Nicoles-MacBook-Air.local>
2018-07-31 02:01:32 +1000

    add Submit Comment Component

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import SubmitComment from './SubmitComment';
 
 
 
@@ -13,6 +14,7 @@ class NoteDetail extends Component {
                 <div className="col-sm-6 col-sm-offset-3">
                 <h1>{note.title}</h1>
                 <p>{note.body}</p>
+                <SubmitComment />
                     <Link to="/">Back</Link>
                     
                 </div>
@@ -27,7 +29,7 @@ function mapStateToProps (state, ownProps) {
     return {
         note: state.notes[ownProps.match.params.id],
         uid: state.user.uid
-    }
+    };
 
 }
 
