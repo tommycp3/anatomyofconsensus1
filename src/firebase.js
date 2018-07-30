f8187d7b8fde7e18fffe6a482009106fa98a89e4
Nicole <nicole@Nicoles-MacBook-Air.local>
2018-07-30 16:08:23 +1000

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDZHsdI5A5x_f75m4MgYZCO7WRft03IkNw",
+    authDomain: "diary-e8b5f.firebaseapp.com",
+    databaseURL: "https://diary-e8b5f.firebaseio.com",
+    projectId: "diary-e8b5f",
+    storageBucket: "diary-e8b5f.appspot.com",
+    messagingSenderId: "321003594246"
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database().ref('/notes');
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const twitterProvider = new firebase.auth.TwitterAuthProvider();
+
+
