21f3ed0ceeb2d737f673cf8cfb299775481841fd
Thomas Miller <thomasmiller@Thomass-MacBook.local>
2018-08-01 14:18:18 +1000

    render comments on NoteDetail page

---
+++ src/components/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comment = (props) => {
+    return (
+        <div>{props.children}</div>
+    )
+}
+
+export default Comment;
--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SubmitComment from './SubmitComment';
+import _ from 'lodash';
+import Comment from './Comment';
 
 
 
 class NoteDetail extends Component {
+    renderComments() {
+        // we get the active note from the props
+        const { note } = this.props;
+        return _.map(note.comments, (comment, key) => {
+            return (
+                <Comment key={key} id={key}>{comment.commentBody}</Comment>
+            )
+        })
+    }
+
     render() {
-        const {note} = this.props;
+        const { note } = this.props;
         return (
             <div className="container-fluid">
                 <div className="row">
-                <div className="col-sm-6 col-sm-offset-3">
-                <h1>{note.title}</h1>
-                <p>{note.body}</p>
-                <SubmitComment id={this.props.match.params.id}/>
-                    <Link to="/">Back</Link>
-                    
-                </div>
+                    <div className="col-sm-6 col-sm-offset-3">
+                        <h1>{note.title}</h1>
+                        <p>{note.body}</p>
+                        <SubmitComment id={this.props.match.params.id} />
+                        {this.renderComments()}
+                        <br />
+                        <Link to="/"> Back</Link>
+
+                    </div>
 
                 </div>
             </div>
@@ -25,7 +39,7 @@ class NoteDetail extends Component {
     };
 };
 
-function mapStateToProps (state, ownProps) {
+function mapStateToProps(state, ownProps) {
     return {
         note: state.notes[ownProps.match.params.id],
         uid: state.user.uid
